3cad88f45de270174b615fc73c553934aa156e39
yugaGit <114376104+yugaGit@users.noreply.github.com>
2022-09-29 21:52:29 +0530

    Add files via upload
    calculator program using operators and method overloading concept.

---
+++ cal.java
@@ -0,0 +1,17 @@
+
+
+//method overloading
+
+ public class cal{
+   
+   public static void main(String[]args){
+   
+    calculator raj=new calculator();
+     raj.add(10,2,3);
+   raj.sub(1,7);
+    raj.mul(10,3);
+     raj.divide(13,2);
+    raj.add(100,20);
+ }
+
+}
---
+++ calculator.java
@@ -0,0 +1,46 @@
+    //method overloading
+
+public class calculator{
+
+ public int add(int a,int b,int c){
+ int d=a+b+c;
+System.out.println(d);
+return d;
+
+}
+
+public  int sub( int j,int k){
+ int z=j-k;
+System.out.println(z);
+ return z;
+
+}
+ public float mul(float m,float n){
+ float a=m*n;
+System.out.println(a);
+return a;
+
+}
+public double divide(double c, double d){
+ double y=c/d;
+System.out.println(y);
+return y;
+}
+
+   //now again using the method name but different variable name 
+
+ public  int add(int x,int y){
+   int z=x+y;
+  System.out.println(z);
+ return z;
+
+
+} 
+
+}
+
+
+
+
+
+
